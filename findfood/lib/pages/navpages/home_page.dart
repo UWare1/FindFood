a81ae2c311297e53eadb9721e6d1b7c40adaf600
Yutthana <yutthana.pi@ku.th>
2022-09-25 05:22:02 +0700

    Update First Section V - 1.1
    Layout Home Page - Finish

--- findfood/lib/pages/navpages/home_page.dart
+++ findfood/lib/pages/navpages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:findfood/misc/colors.dart';
 import 'package:findfood/size_config.dart';
 import 'package:findfood/widgets/app_large_text.dart';
 import 'package:findfood/widgets/app_text.dart';
@@ -12,10 +13,18 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  List photoFoods = [
+    "fried-rice.png",
+    "healthy-meat.png",
+    "salad-with-meat.png"
+  ];
+  List nameFoods = ["Fried Rice", "Healthy Meat", "Salad With Meat"];
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
+    TabController _tabController = TabController(length: 5, vsync: this);
     return Scaffold(
       body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
         //menu text
@@ -57,8 +66,308 @@ class _HomePageState extends State<HomePage> {
         SizedBox(
           height: 30,
         ),
-        //search
+        //tabbar
         Container(
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: TabBar(
+              labelPadding: const EdgeInsets.only(left: 30, right: 20),
+              controller: _tabController,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey.withOpacity(0.8),
+              isScrollable: true,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicator:
+                  CircleTabIndicator(color: AppColors.mainColor, radius: 4),
+              tabs: const [
+                Tab(
+                  text: "Thai",
+                ),
+                Tab(
+                  text: "Japan",
+                ),
+                Tab(
+                  text: "Korea",
+                ),
+                Tab(
+                  text: "Italy",
+                ),
+                Tab(
+                  text: "Others",
+                ),
+              ],
+            ),
+          ),
+        ),
+        //List PhotoFoods
+        Container(
+          padding: const EdgeInsets.only(left: 30),
+          height: 380,
+          width: double.maxFinite,
+          child: TabBarView(
+            controller: _tabController,
+            children: [
+              ListView.builder(
+                itemCount: 3,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    margin:
+                        const EdgeInsets.only(top: 30, bottom: 15, right: 15),
+                    width: 230,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.redAccent,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 6,
+                          offset: Offset(4, 8), // Shadow position
+                        ),
+                      ],
+                      /*image: DecorationImage(
+                            image: AssetImage("assets/images/mountain.jpeg"),
+                            fit: BoxFit.cover)*/
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        //Plate of Food
+                        Container(
+                          width: double.maxFinite,
+                          height: 180,
+                          margin: const EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      "assets/images/" + photoFoods[index]),
+                                  fit: BoxFit.scaleDown)),
+                        ),
+                        //Food Name
+                        Container(
+                          width: double.maxFinite,
+                          margin: EdgeInsets.symmetric(
+                              horizontal: getProportionateScreenWidth(20)),
+                          child: AppLargeText(
+                            text: nameFoods[index],
+                            size: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                        //Star
+                        Container(
+                          width: double.maxFinite,
+                          height: 20,
+                          margin: EdgeInsets.symmetric(
+                              horizontal: getProportionateScreenWidth(20),
+                              vertical: getProportionateScreenWidth(10)),
+                          decoration: BoxDecoration(color: Colors.lightBlue),
+                        ),
+                        //Count & Serve
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              width: 80,
+                              height: 20,
+                              margin: EdgeInsets.only(
+                                  left: getProportionateScreenWidth(20)),
+                              decoration: BoxDecoration(
+                                  color: Color.fromARGB(255, 255, 236, 206)),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    height: double.maxFinite,
+                                    width: 30,
+                                    margin: const EdgeInsets.only(right: 6),
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                "assets/images/mountain.jpeg"),
+                                            fit: BoxFit.cover)),
+                                  ),
+                                  Text("30 min")
+                                ],
+                              ),
+                            ),
+                            Container(
+                              width: 90,
+                              height: 20,
+                              margin: EdgeInsets.only(
+                                  right: getProportionateScreenWidth(20)),
+                              decoration:
+                                  BoxDecoration(color: Colors.lightGreen),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    height: double.maxFinite,
+                                    width: 30,
+                                    margin: const EdgeInsets.only(right: 6),
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                "assets/images/mountain.jpeg"),
+                                            fit: BoxFit.cover)),
+                                  ),
+                                  Text("2 serve")
+                                ],
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+              Text("There"),
+              Text("Bye"),
+              Text("There"),
+              Text("Bye"),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        //Recommended & See all
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: getProportionateScreenWidth(20)),
+              child: AppLargeText(
+                text: "Recommended",
+                size: 24,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(right: getProportionateScreenWidth(20)),
+              child: AppText(
+                text: "See all",
+                size: 16,
+              ),
+            ),
+          ],
+        ),
+        //Recommended Menu
+        Container(
+          width: double.maxFinite,
+          height: SizeConfig.screenHeight * 0.198,
+          child: ListView.builder(
+            itemCount: 3,
+            scrollDirection: Axis.vertical,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                width: double.maxFinite,
+                height: 140,
+                margin: EdgeInsets.only(
+                    top: 8,
+                    left: getProportionateScreenWidth(20),
+                    right: getProportionateScreenWidth(20),
+                    bottom: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.orangeAccent,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 20,
+                      offset: Offset(4, 8), // Shadow position
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 80,
+                      width: 80,
+                      margin: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  "assets/images/healthy-meat.png"))),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AppLargeText(
+                          text: "Healthy Meat",
+                          size: 20,
+                          color: Colors.white,
+                        ),
+                        //Star in Recommended
+                        Container(
+                          width: 200,
+                          height: 20,
+                          margin: EdgeInsets.symmetric(
+                              vertical: getProportionateScreenWidth(4)),
+                          color: Colors.redAccent,
+                        ),
+                        //Count & Serve in Recommended
+                        Row(
+                          children: [
+                            Container(
+                              width: 90,
+                              height: 20,
+                              margin: EdgeInsets.only(
+                                  right: getProportionateScreenWidth(10)),
+                              decoration: BoxDecoration(
+                                  color: Color.fromARGB(255, 255, 236, 206)),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    height: double.maxFinite,
+                                    width: 30,
+                                    margin: const EdgeInsets.only(right: 6),
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                "assets/images/mountain.jpeg"),
+                                            fit: BoxFit.cover)),
+                                  ),
+                                  Text("30 min")
+                                ],
+                              ),
+                            ),
+                            Container(
+                              width: 90,
+                              height: 20,
+                              decoration:
+                                  BoxDecoration(color: Colors.lightGreen),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    height: double.maxFinite,
+                                    width: 30,
+                                    margin: const EdgeInsets.only(right: 6),
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                "assets/images/mountain.jpeg"),
+                                            fit: BoxFit.cover)),
+                                  ),
+                                  Text("2 serve")
+                                ],
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+        //search
+        /*Container(
             child: SingleChildScrollView(
           child: Column(
             children: [
@@ -99,8 +408,35 @@ class _HomePageState extends State<HomePage> {
               )
             ],
           ),
-        ))
+        ))*/
       ]),
     );
+  }
+}
+
+class CircleTabIndicator extends Decoration {
+  final Color color;
+  double radius;
+  CircleTabIndicator({required this.color, required this.radius});
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return _CirclePainter(color: color, radius: radius);
+  }
+}
+
+class _CirclePainter extends BoxPainter {
+  final Color color;
+  double radius;
+  _CirclePainter({required this.color, required this.radius});
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    Paint _paint = Paint();
+    _paint.color = color;
+    _paint.isAntiAlias = true;
+
+    final Offset circleOffset = Offset(
+        configuration.size!.width / 2 - (radius / 2),
+        configuration.size!.height - radius);
+    canvas.drawCircle(offset + circleOffset, radius, _paint);
   }
 }
