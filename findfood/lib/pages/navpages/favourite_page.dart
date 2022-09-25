c089563535efec4e4390d0445baac17d6943e946
Rattanamon <rattanamon.sa@ku.th>
2022-09-26 05:06:49 +0700

    UpdateProfile
    &collection

--- findfood/lib/pages/navpages/favourite_page.dart
+++ findfood/lib/pages/navpages/favourite_page.dart
@@ -1,4 +1,7 @@
+//import 'dart:html';
+
 import 'package:findfood/size_config.dart';
+import 'package:findfood/widgets/app_large_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -14,52 +17,70 @@ class FavouritePage extends StatefulWidget {
 class _FavouritePageState extends State<FavouritePage> {
   @override
   Widget build(BuildContext context) {
-   SizeConfig().init(context);
-    return  Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: getProportionateScreenWidth(10)),
-                child: Column(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                          horizontal: getProportionateScreenWidth(10)),
-                      width: SizeConfig.screenWidth,
-                      height: 50,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: Colors.black54.withOpacity(0.2)),
-                      child: TextField(
-                        maxLines: 1,
-                        onChanged: (value) {
-                          //search value
-                        },
-                        decoration: InputDecoration(
-                            enabledBorder: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                            hintText: "Search FavouriteFood",
-                            hintStyle: const TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                            prefixIcon: const Icon(Icons.search),
-                            prefixIconColor: Colors.black54,
-                            suffixIcon: const Icon(Icons.mic),
-                            suffixIconColor: Colors.black54,
-                            contentPadding: EdgeInsets.symmetric(
-                              horizontal: getProportionateScreenWidth(20),
-                              vertical: getProportionateScreenWidth(12),
-                            )),
-                      ),
-                    )
-                  ],
+    SizeConfig().init(context);
+    return Scaffold(
+      body: ListView(
+        //child: Column(
+        children: [
+          //menu text
+          Container(
+            padding: const EdgeInsets.only(top: 50, left: 20),
+            child: Row(
+              children: [
+                AppLargeText(text: "MyFavouriteFood"),
+                Expanded(child: Container()),
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.withOpacity(0.5)),
                 ),
-              )
-          ],
-        ),
+              ],
+            ),
+          ),
+          //Search
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(10)),
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(
+                      horizontal: getProportionateScreenWidth(10)),
+                  width: 320,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.black54.withOpacity(0.2)),
+                  child: TextField(
+                    maxLines: 1,
+                    onChanged: (value) {
+                      //search value
+                    },
+                    decoration: InputDecoration(
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        hintText: "Search FavoriteFood",
+                        hintStyle: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                        prefixIcon: const Icon(Icons.search),
+                        prefixIconColor: Colors.black54,
+                        suffixIcon: const Icon(Icons.mic),
+                        suffixIconColor: Colors.black54,
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: getProportionateScreenWidth(5),
+                          vertical: getProportionateScreenWidth(12),
+                        )),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+        //),
       ),
-      
     );
   }
 }
--- findfood/lib/pages/navpages/profile_page.dart
+++ findfood/lib/pages/navpages/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:findfood/size_config.dart';
+import 'package:findfood/widgets/app_large_text.dart';
+import 'package:findfood/widgets/app_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -12,6 +14,7 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -25,15 +28,121 @@ class _ProfilePageState extends State<ProfilePage> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Container(
-                width: SizeConfig.screenWidth,
+            padding: const EdgeInsets.only(top: 50, left: 20),
+            child: Row(
+              children: [
+                AppText(text: "Profile"),
+              ],
+            ),
+          ),
+              Container(
+                width: 150,
                 child: const CircleAvatar(
                   radius: 60,
                   backgroundColor: Colors.transparent,
-                  backgroundImage: ExactAssetImage('assets/image/profile.jpg'),
+                  backgroundImage: ExactAssetImage('assets/images/profile.png'),
                 ),
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.black54.withOpacity(.5),
+                    width: 5.0,
+                  )
                 ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+                SizedBox(
+                  width: size.width * .3,
+                  child: Row(
+                    children: [
+                      Text('Smith Shader',style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 20, 
+                      ),
+                      ),
+                      SizedBox(
+                        height: 24.0,
+                        child: Image.asset("assets/images/verify.png"),
+                      )
+                    ],
+                  ),
+                ),
+                Text('Smith@gmail.com',style: TextStyle(
+                    color: Colors.black54.withOpacity(.3),
+                ),),
+              const SizedBox(
+                height: 30.0,
+              ),
+              SizedBox(
+                height: size.height *.7,
+                width: SizeConfig.screenWidth,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.person,color: Colors.black54.withOpacity(.5),),
+                            const SizedBox(
+                              width: 16.0,
+                            ),
+                            Text('MyProfile',style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),)
+                          ],
+                        ),
+                        Icon(Icons.arrow_forward_ios,color: Colors.black54.withOpacity(.4),size: 16.0,),
+                      ],
+                    ),
+                    Text("\n"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.settings,color: Colors.black54.withOpacity(.5),),
+                            const SizedBox(
+                              width: 16.0,
+                            ),
+                            Text('Setting',style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),)
+                          ],
+                        ),
+                        Icon(Icons.arrow_forward_ios,color: Colors.black54.withOpacity(.4),size: 16.0,),
+                      ],
+                    ),
+                    Text("\n"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.logout,color: Colors.black54.withOpacity(.5),),
+                            const SizedBox(
+                              width: 16.0,
+                            ),
+                            Text('Logout',style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),)
+                          ],
+                        ),
+                        Icon(Icons.arrow_forward_ios,color: Colors.black54.withOpacity(.4),size: 16.0,),
+                      ],
+                    ),
+                  ],
+                ),
+
               )
             ],
           ),
@@ -42,3 +151,10 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 }
+
+
+
+
+
+
+
